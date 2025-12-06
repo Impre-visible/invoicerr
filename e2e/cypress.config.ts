d367436bee5d0c07f00b5dcb10005799c684d131
Roméo Chevrier <impr.visible@gmail.com>
2025-12-06 17:46:45 +0100

    🔥 remove: Remove setupNodeEvents function from Cypress configuration

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -5,22 +5,6 @@ export default defineConfig({
   e2e: {
     video: true,
     experimentalStudio: true,
-    setupNodeEvents(on, config) {
-      on('before:run', (details) => {
-        return new Promise((resolve, reject) => {
-          resolve();
-          return
-          exec('node ../backend/prisma/reset-db.test.ts', (err: any, stdout: any, stderr: any) => {
-            if (err) {
-              console.error(stderr);
-              return reject(err);
-            }
-            console.log(stdout);
-            resolve();
-          });
-        });
-      });
-    },
     baseUrl: process.env.FRONTEND_URL || "http://localhost:6284",
     specPattern: "cypress/e2e/**/*.cy.{js,ts}",
     supportFile: "cypress/support/e2e.ts",
